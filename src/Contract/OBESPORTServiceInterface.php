d8aacf426bb4a339be0cfee54fff96de509f21ca
tkd9714 <chunwei.liu@goldenf.co>
2022-10-19 16:47:25 +0800

    add accountToVendor & accountToOperator

--- src/Contract/OBESPORTServiceInterface.php
+++ src/Contract/OBESPORTServiceInterface.php
@@ -80,5 +80,23 @@ interface OBESPORTServiceInterface {
      * @return mixed
      */
     function orderFailCheck(string $op_code, string $order_no);
+
+    /**
+     * 會員帳號轉換(GF->OBESPORT)
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $member_code
+     * @return mixed
+     */
+    function accountToVendor(string $op_code, array $vendor, string $member_code);
+
+    /**
+     * 會員帳號轉換(OBESPORT->GF)
+     * @param string $op_code
+     * @param array $vendor
+     * @param string $vendor_account
+     * @return mixed
+     */
+    function accountToOperator(string $op_code, array $vendor, string $vendor_account);
 }
 
